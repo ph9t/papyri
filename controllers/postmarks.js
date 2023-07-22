a676426256bf99c394192e2a82ae1b353c29705f
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-22 17:19:22 +0800

    feat: add PUT endpoint for 'postmarks' resources

--- controllers/postmarks.js
+++ controllers/postmarks.js
@@ -20,4 +20,27 @@ postmarksRouter.post('/', async (req, res) => {
   res.json(savedPostmark)
 })
 
+postmarksRouter.put('/:id', async (req, res) => {
+  const body = req.body
+
+  if (!(body.title && body.url)) {
+    return res.status(400).json({ error: 'missing required fields' })
+  }
+
+  const postmark = {
+    title: body.title,
+    url: body.url,
+  }
+
+  const updatedPostmark = await Postmark.findByIdAndUpdate(
+    req.params.id,
+    postmark,
+    {
+      new: true,
+    }
+  )
+
+  res.json(updatedPostmark)
+})
+
 module.exports = postmarksRouter
