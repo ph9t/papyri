34e834ba3b41445880a59415ce4cfec0ba4d780b
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-22 17:11:17 +0800

    feat: add POST endpoint for 'postmarks' resources

--- controllers/postmarks.js
+++ controllers/postmarks.js
@@ -7,4 +7,17 @@ postmarksRouter.get('/', async (req, res) => {
   res.json(postmarks)
 })
 
+postmarksRouter.post('/', async (req, res) => {
+  const body = req.body
+
+  if (!(body.title && body.url)) {
+    return res.status(400).json({ error: 'missing required fields' })
+  }
+
+  const postmark = new Postmark({ title: body.title, url: body.url })
+  const savedPostmark = await postmark.save()
+
+  res.json(savedPostmark)
+})
+
 module.exports = postmarksRouter
